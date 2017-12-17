18d02ff1e563e73013b350ff3d267da1a3593382
eonmax <maxwere@wp.pl>
2017-12-17 14:29:03 +0000

    Style errors and msg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
        #render plain: params[:article].inspect
        @article = Article.new(article_params)
        if @article.save
-           flash[:notice] = "Article is created!"
+           flash[:success] = "Article is created!"
            redirect_to article_path(@article)
        else
           render 'new' 
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
    def update
       @article = Article.find(params[:id])
       if @article.update(article_params)
-         flash[:notice]= "Article was sucessfully updated"
+         flash[:success]= "Article was sucessfully updated"
          redirect_to article_path(@article)
       else
          render "edit"
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
    def destroy
       @article = Article.find(params[:id])
       @article.destroy
-      flash[:notice] = "Article was sucessfully deleted"
+      flash[:danger] = "Article was sucessfully deleted"
       redirect_to articles_path
    end
    
