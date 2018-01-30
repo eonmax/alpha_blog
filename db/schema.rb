0fb65e99348a14130bde4ffeca0e5ccff5932066
eonmax <maxwere@wp.pl>
2018-01-30 20:11:31 +0000

    Creat users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171203184723) do
+ActiveRecord::Schema.define(version: 20180130200430) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
